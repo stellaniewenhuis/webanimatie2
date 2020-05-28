b7c54062050cc8cf94b772cf6a6f76ddb9b96947
stellaniewenhuis <45544342+stellaniewenhuis@users.noreply.github.com>
2020-05-29 00:09:35 +0200

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,20 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die momenten komt er een class op de body.
+sounds = [
+    0,
+    9,
+    12,
+    15,
+    16.5,
+    24,
+    25.8,
+    31,
+    32,
+    34,
+    67,
+    75,
+    86,
+    103,
+    105,
+    129,
+];
